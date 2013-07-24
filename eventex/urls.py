1a8b6ab94d3ea9653709ba1e8b43d797aaa333ec
linuxsoares <linux.soares@gmail.com>
2013-07-23 22:02:18 -0700

    mensagem teste para o tutorial

--- eventex/urls.py
+++ eventex/urls.py
@@ -18,9 +18,3 @@ urlpatterns = patterns('eventex.core.views',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),    
 )
-
-urlpatterns += patterns('',
-    (r'^static/(.*)$', 'django.views.static.serve', {
-        'document_root': settings.STATIC_ROOT
-    }),
-)

048d9ffd5d57165650598fb62e3433893ea877f1
linuxsoares <linux.soares@gmail.com>
2013-08-17 13:27:52 -0700

    Course

--- eventex/core/models.py
+++ eventex/core/models.py
@@ -54,3 +54,11 @@ class Talk(models.Model):
 
     def get_absolute_url(self):
         return '/palestras/%d/' % self.pk
+
+class Course(Talk):
+    slots = models.IntegerField()
+    notes = models.TextField()
+
+    objects = PeriodManager()
+
+
--- eventex/core/tests/test_models_talk.py
+++ eventex/core/tests/test_models_talk.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 
 from django.test import TestCase
-from eventex.core.models import Talk
+from eventex.core.models import Talk, Course
 from eventex.core.managers import PeriodManager
 
 class TalkModelTest(TestCase):
@@ -25,3 +25,21 @@ class TalkModelTest(TestCase):
 
     def test_period_manager(self):
         self.assertIsInstance(Talk.objects, PeriodManager)
+
+class CourseModelTest(TestCase):
+    def setUp(self):
+        self.course = Course.objects.create(title=u'Tutorial Django',
+                                            description=u'Descrição do curso', start_time='10:00', slots=20)
+    def test_create(self):
+        self.assertEqual(1, self.course.pk)
+
+    def test_unicode(self):
+        self.assertEqual(u'Tutorial Django', unicode(self.course))
+
+    def test_speakers(self):
+        self.course.speakers.create(name='Gilmar Soares', slug='gilmar-soares',
+                                    url='http://about.me/gilmar.soares')
+        self.assertEqual(1, self.course.speakers.count())
+
+    def test_period_manager(self):
+        self.assertIsInstance(Course.objects, PeriodManager)
